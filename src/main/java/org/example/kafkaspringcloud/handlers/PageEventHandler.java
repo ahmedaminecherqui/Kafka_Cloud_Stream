9ae5ea690b4051bf4832cf00d5304965ed31e5bd
cherqui ahmed amine <cherq@Megan_bias.cherqui2003@gmail.com>
2025-12-12 19:21:35 +0100

    updated event handler with count()

--- src/main/java/org/example/kafkaspringcloud/handlers/PageEventHandler.java
+++ src/main/java/org/example/kafkaspringcloud/handlers/PageEventHandler.java
@@ -1,28 +1,27 @@
 package org.example.kafkaspringcloud.handlers;
 
-
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.example.kafkaspringcloud.events.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import static org.springframework.integration.graph.LinkNode.Type.input;
-
 @Component
 public class PageEventHandler {
 
     @Bean
-    public Consumer<PageEvent> pageEventConsumer(){
-        return (input)->{
+    public Consumer<PageEvent> pageEventConsumer() {
+        return (input) -> {
             System.out.println("*****************");
             System.out.println(input.date());
             System.out.println("*****************");
@@ -30,24 +29,24 @@ public class PageEventHandler {
     }
 
     @Bean
-    public Supplier<PageEvent> pageEventSupplier(){
-        return ()->{
+    public Supplier<PageEvent> pageEventSupplier() {
+        return () -> {
             return new PageEvent(
-                    Math.random()>0.5?"P1":"P2",
-                    Math.random()>0.5?"U1":"U2",
+                    Math.random() > 0.5 ? "P1" : "P2",
+                    Math.random() > 0.5 ? "U1" : "U2",
                     new Date(),
-                    10*new Random().nextInt(10000)
-            );
+                    10 * new Random().nextInt(10000));
         };
     }
 
     @Bean
-    public Function<KStream<String,PageEvent>, KStream<String,Long>> KStreamFunction(){
-        return(input)->
-                input.filter((k,v)->v.duration()>100)
-                        .map((k,v)->new KeyValue<>(v.name(),v.duration()))
-                        .groupByKey(Grouped.with(Serdes.String(),Serdes.Long()))
-                        .count()
-                        .toStream();
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> KStreamFunction() {
+        return (input) -> input.filter((k, v) -> v.duration() > 100)
+                .map((k, v) -> new KeyValue<>(v.name(), v.duration()))
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofSeconds(5)))
+                .count()
+                .toStream()
+                .map((k, v) -> new KeyValue<>(k.key(), v));
     }
 }
